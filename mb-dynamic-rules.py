56d79b951d10d790fc43ab0056ea11f420e2018f
Reginaldo Junior <reginaldo.junior@creditas.com.br>
2023-09-04 13:21:41 -0300

    updates

---
+++ mb-dynamic-rules.py
@@ -0,0 +1,124 @@
+data = {
+    'ARF': {
+        '10': {
+            'airlines': 250,
+            'GMSC': 500,
+            'elecNormNew': 500,
+            'covtypeNorm': 500,
+        },
+        '50': {
+            'airlines': 500,
+            'GMSC': 500,
+            'elecNormNew': 500,
+            'covtypeNorm': 2000,
+        },
+        '90': {
+            'airlines': 250,
+            'GMSC': 2000,
+            'elecNormNew': 500,
+            'covtypeNorm': 2000,
+        }
+    },
+    'LBag': {
+        '10': {
+            'airlines': 2000,
+            'GMSC': 250,
+            'electricity': 500,
+            'covertype': 500,
+        },
+        '50': {
+            'airlines': 50,
+            'GMSC': 500,
+            'electricity': 500,
+            'covertype': 500,
+        },
+        '90': {
+            'airlines': 50,
+            'GMSC': 50,
+            'electricity': 250,
+            'covertype': 500,
+        }
+    },
+    'SRP': {
+        '10': {
+            'airlines': 250,
+            'GMSC': 500,
+            'electricity': 250,
+            'covertype': 25,
+        },
+        '50': {
+            'airlines': 500,
+            'GMSC': 500,
+            'electricity': 500,
+            'covertype': 500,
+        },
+        '90': {
+            'airlines': 500,
+            'GMSC': 500,
+            'electricity': 500,
+            'covertype': 500,
+        }
+    },
+    'OzaBagAdwin': {
+        '10': {
+            'airlines': 500,
+            'GMSC': 250,
+            'electricity': 250,
+            'covertype': 500,
+        },
+        '50': {
+            'airlines': 500,
+            'GMSC': 500,
+            'electricity': 250,
+            'covertype': 500,
+        },
+        '90': {
+            'airlines': 25,
+            'GMSC': 250,
+            'electricity': 250,
+            'covertype': 250,
+        }
+    },
+    'OzaBagASHT': {
+        '10': {
+            'airlines': 250,
+            'GMSC': 500,
+            'electricity': 250,
+            'covertype': 500,
+        },
+        '50': {
+            'airlines': 250,
+            'GMSC': 500,
+            'electricity': 500,
+            'covertype': 500,
+        },
+        '90': {
+            'airlines': 500,
+            'GMSC': 500,
+            'electricity': 250,
+            'covertype': 500,
+        }
+    },
+    'OzaBag': {
+        '10': {
+            'airlines': 500,
+            'GMSC': 500,
+            'electricity': 500,
+            'covertype': 500,
+        },
+        '50': {
+            'airlines': 500,
+            'GMSC': 500,
+            'electricity': 500,
+            'covertype': 500,
+        },
+        '90': {
+            'airlines': 250,
+            'GMSC': 2000,
+            'electricity': 250,
+            'covertype': 500,
+        }
+    }
+}
+
+print(data)
