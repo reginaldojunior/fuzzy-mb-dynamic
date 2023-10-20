49a468091b973330b7991d3584bd296ac9c86ef0
Reginaldo Junior <76regi@gmail.com>
2023-10-20 18:53:47 -0300

    chore: updates dynamic socket with percent random

--- SocketJavaDynamic.java
+++ SocketJavaDynamic.java
@@ -1,44 +1,55 @@
-import java.nio.file.*;
-import java.security.acl.LastOwnerException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.List;
 import java.util.Random;
-import java.io.IOException;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.io.*;
-import java.net.ConnectException;
-import java.lang.InterruptedException;
 import java.util.concurrent.TimeUnit;
-import java.net.InetAddress;
-import java.net.ServerSocket;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.channels.AsynchronousServerSocketChannel;
-import java.nio.channels.AsynchronousSocketChannel;
-import java.util.concurrent.Future;
-import java.nio.channels.SocketChannel;
-import java.nio.channels.ServerSocketChannel;
 
 public class SocketJavaDynamic {
+    private static long lastMinuteUpdate = 0;
+    private static int ips = 0;
 
-    public static int oscilarInstanciasPorSegundo(int limit) {
-        double limitPerSegundo = limit * 0.05;
+    public static boolean passouUmMinuto(long started, long current) {
+        double elapsedSeconds = (current - started) / 1000.0;
 
-        Random random = new Random();
-        int ips = random.nextInt((int) limitPerSegundo) + 1;
+        System.out.println("Elapsed Seconds: " + elapsedSeconds);
 
-        return ips;
+        return elapsedSeconds > 60;
     }
 
-    public static int oscilarTempoEntreEnvioDeMensagens(int limit) {
-        Random random = new Random();
-        int segundos = random.nextInt(limit);
+    public static int oscilarInstanciasPorSegundo(int limit, int maxRate, long started, long current) {
+        final double PERCENT_10 = 0.10;
+        final double PERCENT_50 = 0.50;
+        final double PERCENT_90 = 0.90;
 
-        return segundos;
+        if (passouUmMinuto(lastMinuteUpdate, current)) {
+            Random random = new Random();
+            int choice = random.nextInt(3);
+            if (choice == 0) {
+                ips = (int) (maxRate * PERCENT_10);
+            } else if (choice == 1) {
+                ips = (int) (maxRate * PERCENT_50);
+            } else {
+                ips = (int) (maxRate * PERCENT_90);
+            }
+            lastMinuteUpdate = current;
+        }
+
+        System.out.println("Limit Per Second: " + ips);
+
+        // Gere um número aleatório com base no limite por segundo
+        Random random = new Random();
+        int ipsToSend = random.nextInt(ips);
+
+        return ipsToSend;
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        if (args.length != 3) {
+        if (args.length != 4) {
             System.out.println("Usage: ServerProducer <ip> <port> <file> <Rate of instances per second>");
             System.exit(1);
         }
@@ -46,10 +57,10 @@ public class SocketJavaDynamic {
         String ip = args[0];
         int port = Integer.parseInt(args[1]);
         String filename = args[2];
+        int maxRate = Integer.parseInt(args[3]);
 
         int instancesSent = 0;
 
-        // load all file to memory
         List<String> lines = Files.readAllLines(Paths.get(filename));
         System.out.println("read " + lines.size() + " lines to memory from file " + filename + ".");
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
@@ -57,49 +68,46 @@ public class SocketJavaDynamic {
         serverSocketChannel.bind(new InetSocketAddress(ip, port));
         System.out.println("ServerSocketChannel awaiting connections..." + serverSocketChannel.getLocalAddress());
         int i = 0;
-        SocketChannel socketChannel = serverSocketChannel.accept(); // blocking
+
+        SocketChannel socketChannel = serverSocketChannel.accept();
         System.out.println("Connection from " + socketChannel + "!");
 
         serverSocketChannel.configureBlocking(false);
+        boolean minuteComplete = false;
+
         if (socketChannel != null) {
             socketChannel.configureBlocking(false);
 
-            boolean reading_header = true;
-            while (reading_header) {
-                String l = lines.get(i);
-                if (l.contains("@data"))
-                    reading_header = false;
+            boolean readingHeader = true;
+            while (readingHeader) {
+                String line = lines.get(i);
+                if (line.contains("@data")) {
+                    readingHeader = false;
+                }
                 i++;
             }
 
             String msg = "";
-
-            boolean keep_going = true;
-
+            boolean keepGoing = true;
             long startTime = 0;
-            ByteBuffer buffer = ByteBuffer.allocate(1048576); //1048576
-
+            ByteBuffer buffer = ByteBuffer.allocate(1048576);
             int limit = lines.size();
 
             long startingAll = System.currentTimeMillis();
-            long lastSuccessfullStep = 0;
-            int ips = 0;
+            long countMinute = System.currentTimeMillis();
             int ipb = 0;
-            int segundos = 0;
-            while (i < limit && keep_going) {
-                segundos = 0;
+
+            while (i < limit && keepGoing) {
                 startTime = System.nanoTime();
-                // We want to send a fixed per second rate, but divided in smaller chunks
-                // we use IPB (IPS/5) to send 5 smaller chunks
-                // create the chunk by sending instances without delay (Strings)
-                // while (instances.size() < IPB && i < lines.size()) {
-                // send chunk
+                ipb = oscilarInstanciasPorSegundo(limit, maxRate, countMinute, System.currentTimeMillis());
 
-                ips = oscilarInstanciasPorSegundo(limit);
-                ipb = ips / 5;
+                if (passouUmMinuto(countMinute, System.currentTimeMillis())) {
+                    countMinute = System.currentTimeMillis();
+                }
 
-                int num_inst = 0;
-                while (num_inst < ipb && i < limit) {                    
+                int numInst = 0;
+
+                while (numInst < ipb && i < limit) {
                     msg = lines.get(i++) + "#";
                     buffer.flip();
                     buffer = ByteBuffer.wrap(msg.getBytes());
@@ -108,46 +116,44 @@ public class SocketJavaDynamic {
                         socketChannel.write(buffer);
                     } catch (IOException ex) {
                         System.out.println("Closed by client!");
-                        keep_going = false;
+                        keepGoing = false;
                         break;
                     }
 
-                    num_inst++;
+                    numInst++;
                     instancesSent++;
                 }
 
-                //System.out.println("Sent [ " + (num_inst) + " ] messages.");
-
                 long justSent = System.nanoTime();
                 long elapsed = TimeUnit.NANOSECONDS.toMillis(justSent - startTime);
-                long sl = elapsed > 0 && elapsed < 1000 ? 1000 - elapsed : 1000;
+                long sleepTime = elapsed > 0 && elapsed < 1000 ? 1000 - elapsed : 1000;
 
-                Thread.sleep(sl);
+                Thread.sleep(sleepTime);
 
-                if (!keep_going)
+                if (!keepGoing) {
                     break;
+                }
 
-                if ((System.currentTimeMillis() - startingAll) / 1000F > 120) {
-                    keep_going = false;
+                if ((System.currentTimeMillis() - startingAll) / 1000F > 600) {
+                    keepGoing = false;
                 }
             }
 
             double totalSpent = (System.currentTimeMillis() - startingAll) / 1000.0;
 
-            // send finish message
-            if (keep_going) {
+            if (keepGoing) {
                 msg = "$$";
                 buffer.flip();
                 buffer = ByteBuffer.wrap(msg.getBytes());
-                if (socketChannel.isOpen())
+                if (socketChannel.isOpen()) {
                     socketChannel.write(buffer);
+                }
             }
 
             System.out.println("\nTotal Time Producer (s): " + totalSpent);
             System.out.println("Total instances Producer: " + instancesSent);
-            System.out.println("Producer Rate (inst per second): " + instancesSent/totalSpent);
+            System.out.println("Producer Rate (inst per second): " + instancesSent / totalSpent);
 
-            // close socket
             System.out.println("Closing socket and terminating program.");
             socketChannel.configureBlocking(true);
             socketChannel.close();
